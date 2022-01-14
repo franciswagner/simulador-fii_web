7787d8bde99e29518051e5eeba38224dd7351afa
Guilherme Garcia Rosa Dias <50960623+Guigarciarosa@users.noreply.github.com>
2022-01-13 22:30:11 -0300

    commit on main

--- app/models/tables.py
+++ app/models/tables.py
@@ -2,4 +2,17 @@ from app import db
 
 class User(db.Model):
     __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
+    conteudo_s_n = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, nome, email, idade, conteudo_s_n):
+        self.nome = nome
+        self.email = email
+        self.idade = idade
+        self.conteudo_s_n = conteudo_s_n
     
+    def __repr__(self):
+        return "<User %r>" % self.nome
