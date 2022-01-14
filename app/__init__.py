88e178384ecad73c2e2a2909a422815f3b18d3c1
Guilherme Garcia Rosa Dias <50960623+Guigarciarosa@users.noreply.github.com>
2022-01-13 22:29:35 -0300

    commit on main

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite//simulator_database.db'
+db = SQLAlchemy(app)
 
 from app.controllers import default
